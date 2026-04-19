79bccbc1b8a71ea19c8cab6459d23004ff570335
Ethan Ayache <ethan.ayache@ortmontreuil.fr>
2026-04-19 13:18:44 +0200

    ajout admin

---
+++ api/get-all-invites.js
@@ -0,0 +1,21 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+)
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  const { data, error } = await supabase
+    .from('invites')
+    .select('nom, places, token, rsvp')
+    .order('nom', { ascending: true })
+
+  if (error) {
+    return res.status(500).json({ error: 'Erreur serveur' })
+  }
+
+  res.status(200).json(data)
+}
